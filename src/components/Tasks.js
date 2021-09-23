cf9454416b5b233d2d2df52e327e49ff9b9059f0
elvstejd <elvistejeda0910@gmail.com>
2021-09-23 18:09:15 -0400

    Create Tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { useTasks } from '../contexts/TasksContext';
+import Task from './Task';
+import TaskAddInput from './TaskAddInput';
+
+function Tasks() {
+    const [isAddingTask, setIsAddingTask] = useState(false);
+    const { tasks, taskOrder, setTaskOrder } = useTasks();
+
+    function onDragEnd(result) {
+        const { destination, source, draggableId } = result;
+        if (!destination) {
+            return;
+        }
+        if (destination.index === source.index) {
+            return;
+        }
+
+        const prevTaskOrder = taskOrder;
+        const newTaskOrder = Array.from(prevTaskOrder);
+        newTaskOrder.splice(source.index, 1);
+        newTaskOrder.splice(destination.index, 0, draggableId);
+
+        setTaskOrder(newTaskOrder);
+    }
+
+    function openInput() {
+        setIsAddingTask(true);
+    }
+
+    function closeInput() {
+        setIsAddingTask(false);
+    }
+
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="tasks">
+                {(provided, snapshot) => (
+                    <div
+                        className="task-container"
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                    >
+                        {taskOrder.map((taskId, index) => {
+                            const task = tasks[taskId];
+                            return (
+                                <Task
+                                    key={task.id}
+                                    currentTask={task}
+                                    index={index}
+                                />
+                            );
+                        })}
+                        {provided.placeholder}
+                        {isAddingTask && <TaskAddInput onFocusOut={closeInput} />}
+                        <div className="new-task-add" onClick={openInput}>
+                            <p>+ Add new task</p>
+                        </div>
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
+
+}
+
+export default Tasks;
