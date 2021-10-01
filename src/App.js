08ae0c9dc9a80d38f875cbe9b7c87a67e19aa536
elvstejd <elvistejeda0910@gmail.com>
2021-10-01 13:46:36 -0400

    Move header component outside main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import TasksProvider from './contexts/TasksContext';
 import CountdownProvider from './contexts/countdownContext';
 import MainPage from './pages/MainPage';
+import Header from './components/Header';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <TasksProvider>
         <div>
           <Router>
+            <Header />
             <Switch>
               <Route exact path="/" component={MainPage} />
             </Switch>
