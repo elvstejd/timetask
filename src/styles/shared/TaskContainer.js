4a4ed75bc30648e2fafd5fc83f07eb17315f34ab
elvstejd <elvistejeda0910@gmail.com>
2022-02-14 22:29:56 -0400

    Add media query to task container

--- src/styles/shared/TaskContainer.js
+++ src/styles/shared/TaskContainer.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 export const TaskContainer = styled.div`
-    width: 40rem;
+    min-width: 40rem;
     background-color: var(--gray-90);
     padding: 1rem 1rem 0 .1rem;
     overflow: auto;
     border-radius: var(--border-radius-lg);
+
+    @media (max-width: 675px) {
+        min-width: 92vw;
+    }
 `;
